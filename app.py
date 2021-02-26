d6a10e5a8d5dc82b054c0ecb18fb038a9ea3459c
julia-ing <ywchoi0625@gmail.com>
2021-02-26 13:08:10 +0900

    pages only for specific user

--- app.py
+++ app.py
@@ -1,33 +1,82 @@
 from datetime import datetime
-
 from bson import ObjectId
-from flask import Flask, render_template, jsonify, request, redirect, url_for
-
-app = Flask(__name__)
-
-# branch: temp - 03
+from flask import Flask, render_template, jsonify, request, redirect, url_for, flash, session
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+from multiprocessing import Process
 
 import re
+import time
 import requests
-from bs4 import BeautifulSoup
 
-from pymongo import MongoClient
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 
 client = MongoClient('localhost', 27017)
 db = client.dbmakers
 
 my_date = datetime.now().strftime("%d")
 
-
 @app.route('/')
 def home():
-    return render_template('home.html')
+    if session['logged_in']:
+        return render_template('home2.html')
+    else:
+        return render_template('home.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template("register.html")
+    else:
+        username = request.form.get('username')
+        email = request.form.get('email')
+        userid = request.form.get('userid')
+        password = request.form.get('password')
+        re_password = request.form.get('re_password')
+
+        userinfo = {'user_id': userid, 'user_name': username, 'user_pwd': password, 'user_email': email}
+
+        if not (userid and username and password and re_password):
+            flash("모두 입력해주세요")
+        elif password != re_password:
+            flash("비밀번호를 확인해주세요")
+        else:
+            db.users.insert_one(userinfo)
+            flash("회원가입 완료")
+        return redirect('/register')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        user_id = request.form['id_input']
+        session['user'] = user_id
+        user_pw = request.form['pw_input']
+
+        user = db.users.find_one({'user_id': user_id}, {'user_pwd': user_pw})
+        if user is not None:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        else:
+            flash("로그인 실패")
+            return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return redirect('/')
 
 
 @app.route('/news')
 def news():
     req = requests.get('https://www.boannews.com/media/list.asp')
     soup = BeautifulSoup(req.text, 'html.parser')
+
     myList = []
     items = soup.findAll("div", {"class": "news_list"})
 
@@ -51,10 +100,11 @@ def news():
             myList.append(item_info)
     req2 = requests.get('https://www.boannews.com/media/o_list.asp')
     soup2 = BeautifulSoup(req2.text, 'html.parser')
+
     week_list = []
     week_items = soup2.findAll("div", {"class": "news_list"})
 
-    for item in week_items:
+    for item in week_items[:10]:
         title = item.select_one("span", {"class": "news_txt"}).text
         title_url = item.find("a")["href"]
         sub_str = "https://www.boannews.com" + title_url
@@ -69,7 +119,8 @@ def news():
 
 @app.route('/contest')
 def contest():
-    req = requests.get('https://www.contestkorea.com/sub/list.php?displayrow=12&Txt_sGn=1&Txt_key=all&Txt_word=&Txt_bcode=030210001&Txt_code1=&Txt_aarea=&Txt_area=&Txt_sortkey=a.str_aedate&Txt_sortword=desc&Txt_chocode=&Txt_unicode')
+    req = requests.get(
+        'https://www.contestkorea.com/sub/list.php?displayrow=12&Txt_sGn=1&Txt_key=all&Txt_word=&Txt_bcode=030210001&Txt_code1=&Txt_aarea=&Txt_area=&Txt_sortkey=a.str_aedate&Txt_sortword=desc&Txt_chocode=&Txt_unicode')
     soup = BeautifulSoup(req.text, 'html.parser')
     myList = []
     items = soup.select('#frm > div > div.list_style_2 > ul > li')
@@ -95,14 +146,17 @@ def contest():
     return render_template('contest.html', list=myList)
 
 
-@app.route('/job')
-def job():
-    return render_template('job.html')
+@app.route('/job/<user>')
+def job(user):
+    if session['logged_in']:
+        return render_template('job.html')
+    else:
+        return render_template('login_required.html')
 
 
 @app.route('/job/memo', methods=['GET'])
 def job_listing():
-    jobs = list(db.jobs.find({}, {'_id': False}))
+    jobs = list(db.jobs.find({'author': session['user']}, {'_id': False}))
     return jsonify({'jobs': jobs})
 
 
@@ -117,20 +171,25 @@ def job_saving():
 
     title = soup.select_one('meta[property="og:title"]')['content']
     image = soup.select_one('meta[property="og:image"]')['content']
+    author = session['user']
 
     doc = {
         'title': title,
         'image': image,
         'url': url_receive,
-        'memo': memo_receive
+        'memo': memo_receive,
+        'author': author
     }
     db.jobs.insert_one(doc)
     return jsonify({'msg': '저장이 완료되었습니다!'})
 
 
-@app.route('/study', methods=['GET'])
-def study():
-    return render_template("study.html")
+@app.route('/study/<user>', methods=['GET'])
+def study(user):
+    if session['logged_in']:
+        return render_template('study.html')
+    else:
+        return render_template('login_required.html')
 
 
 @app.route('/study/<category>/sub', methods=['GET'])
@@ -142,7 +201,7 @@ def study_sub(category):
 
 @app.route('/study/add', methods=['GET'])
 def study_listing():
-    categories = list(db.categories.find({}, {'_id': False}))
+    categories = list(db.categories.find({'author': session['user']}, {'_id': False}))
     return jsonify({'categories': categories})
 
 
@@ -150,9 +209,11 @@ def study_listing():
 def study_saving():
     category_receive = request.form['category_give']
     level_receive = request.form['level_give']
+    author = session['user']
     doc = {
         'category': category_receive,
         'level': int(level_receive),
+        'author': author
     }
     db.categories.insert_one(doc)
     return jsonify({'msg': '저장이 완료되었습니다!'})
